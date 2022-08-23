960f7de5c70cc695c8294e9737507f4fe5336d7d
Manuelacostar97 <manuelacostar97@gmail.com>
2022-08-23 03:39:06 +0000

    Footer

--- src/js/component/Footer.jsx
+++ src/js/component/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const Footer = (props) => {
     return(
-        <footer className="bg-dark text-light d-flex justify-content-center position-relative py-3 my-4 border-top ">
+        <footer className="bg-dark text-light d-flex justify-content-center">
             <p className="my-2">Copyright © Your Website 2022</p>
         </footer>
     );
